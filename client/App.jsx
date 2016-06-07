a3176afdb8e418f97b9077b3440b123434f6f6e6
= <=>
2016-06-07 17:55:53 +0300

    update

---
+++ client/App.jsx
@@ -0,0 +1,26 @@
+/**
+ * Root Component
+ */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
+// Import Routes
+import routes from './routes';
+
+// Base stylesheet
+//require('./main.css');
+
+export default function App(props) {
+  return (
+    <Provider store={props.store}>
+      <Router history={browserHistory}>
+        {routes}
+      </Router>
+    </Provider>
+  );
+}
+
+App.propTypes = {
+  store: React.PropTypes.object.isRequired
+};
