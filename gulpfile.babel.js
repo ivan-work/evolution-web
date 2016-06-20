605ea155a75d11591abe589a1d61ca03f2cf12e8
Fen1kz <ffen1kz@gmail.com>
2016-06-21 01:53:58 +0300

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,17 +3,31 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const tape = require('gulp-tape');
+const mocha = require('gulp-mocha');
 const watch = require('gulp-watch');
 const tapReporter = require('tap-spec');
 
-const testWatch = (path) => gulp.src(path)
-  .pipe(tape({
-    reporter: tapReporter()
-    , bail: true
-  }))
-  .on('error', gutil.log);
+//const testWatch = (path) => gulp.src(['./shared/test-helper.js', path])
+//  .pipe(tape({
+//    reporter: tapReporter()
+//    , bail: true
+//  }))
+//  .on('error', () => console.error('error'));
 
-gulp.task('test:shared:watch', () => {
-  testWatch('shared/**/*.spec.js');
-  return watch('shared/**/*.js', () => testWatch('shared/**/*.spec.js'));
+const testWatch = (paths) => gulp.src(paths)
+  .pipe(mocha({
+    bail: true
+  }))
+  .on('error', () => console.error('error'));
+
+gulp.task('test:client:watch', () => {
+  const paths = ['./shared/test-helper.js', 'shared/**/*.spec.js', 'client/**/*.spec.js'];
+  testWatch(paths);
+  return watch('{shared, client}/**/*.js', () => testWatch(paths));
+});
+
+gulp.task('test:server:watch', () => {
+  const paths = ['./shared/test-helper.js', 'shared/**/*.spec.js', 'server/**/*.spec.js'];
+  testWatch(paths);
+  return watch('{shared, server}/**/*.js', () => testWatch(paths));
 });
