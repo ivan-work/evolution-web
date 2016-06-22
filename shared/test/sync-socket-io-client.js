866e442496c92ca3f50ab556b035ee32e562cdf9
= <=>
2016-06-22 20:12:45 +0300

    update

--- shared/test/sync-socket-io-client.js
+++ shared/test/sync-socket-io-client.js
@@ -1,5 +1,24 @@
-class SyncSocketIOClient {
+import EventEmitter from 'events';
 
+class SyncSocketIOClient extends EventEmitter {
+  constructor(server) {
+    super();
+    this.server = server;
+  }
+
+  connect(server) {
+    this.server = server || this.server;
+    this.connected = true;
+    this.emit('connect');
+    this.server.emit('connect', this);
+    this.server.emit('connection', this);
+  }
+
+  disconnect(reason) {
+    this.emit('disconnect')
+  }
 }
 
-export default new SyncSocketIOClient;
+export default (...args) => new SyncSocketIOClient(...args);
+
+export const SyncSocket = SyncSocketIOClient;
--- shared/test/sync-socket-io.js
+++ shared/test/sync-socket-io.js
@@ -1,5 +1,20 @@
-class SyncSocketIO {
+import EventEmitter from 'events';
 
+class SyncSocketIO extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  listen(cb) {
+    cb();
+  }
+
+  of(namespace, onConnect) {
+    if (onConnect) this.on('connect', onConnect);
+    return this;
+  }
 }
 
-export default new SyncSocketIO;
+export default (...args) => new SyncSocketIO(...args);
+
+export const SyncSocketServer = SyncSocketIO;
