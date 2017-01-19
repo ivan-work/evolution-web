2a7d12b9f7041f52296cc6085d931eb80cf9d5cf
Fen1kz <ffen1kz@gmail.com>
2017-01-19 16:51:50 +0300

    bugfix

--- client/views/game/cards/Deck.jsx
+++ client/views/game/cards/Deck.jsx
@@ -1,7 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import RIP from 'react-immutable-proptypes';
 
-export default class Deck extends Component {
+// Validation
+import {CardModel} from '../../../../shared/models/game/CardModel';
+
+import {AnimationServiceRef} from '../../../services/AnimationService';
+import {Card} from './Card.jsx';
+
+import './Deck.scss';
+
+export class Deck extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
@@ -15,7 +24,7 @@ export default class Deck extends Component {
   }
 
   render() {
-    return <div className={`Deck`}>
+    return <div className='Deck'>
       {React.Children.map(this.props.children, (card, index) => this.renderCardPlace(card, index))}
     </div>;
   }
@@ -25,9 +34,23 @@ export default class Deck extends Component {
     return <div className="CardPlace" key={index} style={{
         left: transform.x + 'px'
         , top: transform.y + 'px'
+        , position: 0 === index ? 'relative' : 'absolute'
           //transform: `translate(${transform.x}px,${transform.y}px)`
         }}>
       {card}
     </div>
   }
 }
+
+export const DeckWrapper = AnimationServiceRef(({deck, connectRef}) => (
+  <Deck ref={connectRef('Deck')}>
+    {deck.map((cardModel, i) => <Card key={i} card={cardModel}/>)}
+  </Deck>
+));
+
+DeckWrapper.propTypes = {
+  deck: RIP.listOf(React.PropTypes.instanceOf(CardModel)).isRequired
+  //, connectRef: PropTypes.func.isRequired
+};
+
+export default DeckWrapper;
