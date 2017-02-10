91f8169725bccdd155754a4bb0a7e6ac28cefc05
Fen1kz <ffen1kz@gmail.com>
2017-02-10 15:48:28 +0300

    ambush-2

--- shared/actions/ttf-specs/trait.ambush.spec.js
+++ shared/actions/ttf-specs/trait.ambush.spec.js
@@ -25,4 +25,22 @@ players:
     expect(selectAnimal(User0, 0).id, '$A should be dead').equal('$B');
     expect(selectAnimal(User1, 0).getFood(), '$C should ambush $A').equal(2);
   });
+
+  it('Simple defence', () => {
+    const [{serverStore, ParseGame}, {clientStore0, User0, ClientGame0}, {User1}] = mockGame(2);
+    const gameId = ParseGame(`
+deck: 10 camo
+phase: 2
+food: 4
+players:
+  - continent: $A tail, $B hiber
+  - continent: $C ambush=true carn
+`);
+    const {selectGame, selectPlayer, selectCard, selectAnimal} = makeGameSelectors(serverStore.getState, gameId);
+
+    clientStore0.dispatch(traitTakeFoodRequest('$A'));
+    expect(selectAnimal(User1, 0).getFood(), '$C should get $A tail').equal(1);
+    expect(selectAnimal(User0, 0).id, '$A should be alive').equal('$A');
+    expect(selectAnimal(User0, 0).getFood(), '$A should get food').equal(1);
+  });
 });
