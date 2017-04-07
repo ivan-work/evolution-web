26c8b89038a74725ef7949d233eaac9742bf983d
ivan <ivan.work.spb@gmail.com>
2017-04-07 03:15:28 +0300

    random-room

--- shared/actions/actions.js
+++ shared/actions/actions.js
@@ -1,11 +1,17 @@
 export * from './auth';
+export * from './rooms';
 
 import {authClientToServer} from './auth';
 import {authServerToClient} from './auth';
+import {roomsClientToServer} from './rooms';
+import {roomsServerToClient} from './rooms';
+
 export const clientToServer = Object.assign({}
   , authClientToServer
+  , roomsClientToServer
 );
 
 export const serverToClient = Object.assign({}
   , authServerToClient
+  , roomsServerToClient
 );
