8d16db1b6c7ed7ed6d19b4dbaf939d3d9ee5d12b
ivan <ivan.work.spb@gmail.com>
2017-04-07 03:15:25 +0300

    update

--- shared/actions/auth.spec.js
+++ shared/actions/auth.spec.js
@@ -1,26 +1,43 @@
-import test from 'tape';
-
 import {Map, fromJS} from 'immutable';
 import {UserRecord} from '../models/User';
 import {authServerToClient, authClientToServer, socketConnect, socketDisconnect, loginUserRequest, loginUserSuccess, loginUserFailure} from './auth';
 
-test('Auth testing', (assert) => {
-  assert.test('socketConnect', () => {
-
+describe('Auth testing', () => {
+  it('socketConnect', () => {
   });
-  assert.test('socketDisconnect', () => {
+
+  it('socketDisconnect', () => {
     const store = mockStore(fromJS({
       users: {
         0: new UserRecord({id: 0, connectionId: '0'})
         , 1: new UserRecord({id: 1, connectionId: '1'})
       }
     }));
-  //  store.dispatch(socketDisconnect('1'));git 
-  //  expect(store.getActions()).eql([
+
+    store.dispatch(socketDisconnect('1'));
+
+    expect(store.getActions()).eql([
+      {type: 'logoutUser', data: 1}
+      , {type: 'socketDisconnect', data: {connectionId: '1'}}
+    ]);
+  });
+  //t.test('socketConnect', (t) => {
+  //  t.end();
+  //});
+  //t.test('socketDisconnect', (t) => {
+  //  const store = mockStore(fromJS({
+  //    users: {
+  //      0: new UserRecord({id: 0, connectionId: '0'})
+  //      , 1: new UserRecord({id: 1, connectionId: '1'})
+  //    }
+  //  }));
+  //  store.dispatch(socketDisconnect('1'));
+  //  t.deepEqual(store.getActions(), [
   //    {type: 'logoutUser', data: 1}
   //    , {type: 'socketDisconnect', data: {connectionId: '1'}}
   //  ]);
-  });
+  //  t.end();
+  //});
   //describe('loginUserRequest', () => {
   //  it('clientSide', () => {
   //  });
