9629d0dafc211d790821d848ae2f9bdac6f4a919
ivan <ivan.work.spb@gmail.com>
2017-04-07 03:16:41 +0300

    fat-as-prop-test

---
+++ shared/models/game/evolution/AnimalModel.spec.js
@@ -0,0 +1,16 @@
+import {Map} from 'immutable';
+import {AnimalModel} from './AnimalModel';
+import {TraitModel} from './TraitModel';
+import {TraitFatTissue} from './traitTypes';
+
+describe('AnimalModel', () => {
+  it.only('Food', () => {
+    const animalMap = Map({animal: AnimalModel.new('user')})
+      .update('animal', animal =>
+        animal.traitAdd(TraitModel.new(TraitFatTissue)
+          .set('value', true)));
+
+    console.log(animalMap.getIn(['animal']).fat)
+    console.log(animalMap.getIn(['animal', 'fat']))
+  });
+});
