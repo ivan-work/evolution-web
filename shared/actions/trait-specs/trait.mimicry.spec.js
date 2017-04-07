2f9147a4440003e8bfcd5458538c5768a5eb7e7d
ivan <ivan.work.spb@gmail.com>
2017-04-07 03:15:58 +0300

    mimicry-advanced

--- shared/actions/trait-specs/trait.mimicry.spec.js
+++ shared/actions/trait-specs/trait.mimicry.spec.js
@@ -13,7 +13,7 @@ import {makeGameSelectors} from '../../selectors';
 
 describe.only('TraitMimicry:', () => {
   describe('Feeding:', () => {
-    it('Simple', () => {
+    it('$A > $B > $C', () => {
       const [{serverStore, ServerGame, ParseGame}, {clientStore0, User0, ClientGame0}, {clientStore1, User1, ClientGame1}] = mockGame(2);
       const gameId = ParseGame(`
 phase: 2
@@ -42,23 +42,40 @@ players:
       expect(selectAnimal(User1, 0).id).equal('$B');
       expect(selectAnimal(User1, 1)).undefined;
     });
+    it.only('$A > $B > $C > $B', () => {
+      const [{serverStore, ServerGame, ParseGame}, {clientStore0, User0, ClientGame0}, {clientStore1, User1, ClientGame1}] = mockGame(2);
+      const gameId = ParseGame(`
+phase: 2
+players:
+  - continent: $A carn
+  - continent: $B mimicry, $C mimicry
+`);
+      const {selectGame, selectPlayer, selectAnimal, selectTrait} = makeGameSelectors(serverStore.getState, gameId);
+      const {activateTrait} = makeGameActionHelpers(serverStore.getState, gameId);
 
-//    it('Dodges attack', () => {
-//      const [{serverStore, ServerGame, ParseGame}, {clientStore0, User0, ClientGame0}, {clientStore1, User1, ClientGame1}] = mockGame(2);
-//      const gameId = ParseGame(`
-//phase: 2
-//players:
-//  - continent: $ carn
-//  - continent: $ mimicry, $
-//`);
-//      const {selectGame, selectPlayer, selectAnimal, selectTrait} = makeGameSelectors(serverStore.getState, gameId);
-//      const {activateTrait} = makeGameActionHelpers(serverStore.getState, gameId);
-//
-//      expect(selectTrait(User1, 0, 0).type).equal('TraitMimicry');
-//
-//      clientStore0.dispatch(activateTrait(User0, 0, 'TraitCarnivorous', User1, 0));
-//
-//      clientStore1.dispatch(gameEndTurnRequest());
-//    });
+      expect(selectTrait(User1, 0, 0).type).equal('TraitMimicry');
+
+      //console.log(selectAnimal(User0, 0))
+      //console.log(selectAnimal(User1, 0))
+      //console.log(selectAnimal(User1, 1))
+
+      clientStore0.dispatch(activateTrait(User0, 0, 'TraitCarnivorous', User1, 0));
+
+      clientStore1.dispatch(traitMimicryAnswer(
+        User0.id, '$A'
+        , 'TraitCarnivorous'
+        , User1.id, '$B'
+        , User1.id, '$C'));
+
+      clientStore1.dispatch(traitMimicryAnswer(
+        User0.id, '$A'
+        , 'TraitCarnivorous'
+        , User1.id, '$C'
+        , User1.id, '$B'));
+
+      expect(selectAnimal(User0, 0).getFood()).equal(2);
+      expect(selectAnimal(User1, 0).id).equal('$C');
+      expect(selectAnimal(User1, 1)).undefined;
+    });
   });
 });
