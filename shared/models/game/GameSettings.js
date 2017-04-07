20bf68aef742142defda406eb8c9ad408dabee34
ivan <ivan.work.spb@gmail.com>
2017-04-07 03:16:58 +0300

    pre-release-bugfixes

--- shared/models/game/GameSettings.js
+++ shared/models/game/GameSettings.js
@@ -84,5 +84,42 @@ export const Deck_Base = [
 ];
 
 export const Deck_TimeToFly = [
-
+//0
+//полёт / хищник - 2,
+//полёт / специализация А - 2,
+//полёт / специализация В - 2,
+//интеллект / жировой запас - 2,
+  [2, cardsData.CardFlightAndCarnivorous.type]
+  , [2, cardsData.CardFlightAndSpecA.type]
+  , [2, cardsData.CardFlightAndSpecB.type]
+  , [2, cardsData.CardIntellectAndFatTissue.type]
+//1
+//интеллект / специализация А - 2,
+//засада / специализация В - 2,
+//засада / водоплавающее - 2,
+//удильщик - 4,
+  , [2, cardsData.CardIntellectAndSpecA.type]
+  , [2, cardsData.CardAmbushAndSpecB.type]
+  , [2, cardsData.CardAmbushAndSwimming.type]
+  , [4, cardsData.CardAnglerfish.type]
+//2
+//чернильное облако - 4,
+//раковина - 4,
+//метаморфоза / хищник - 2,
+//метаморфоза / специализация А - 2,
+//живорождение / специализация В - 2,
+  , [4, cardsData.CardInkCloud.type]
+  , [4, cardsData.CardShell.type]
+  , [2, cardsData.CardMetamorphoseAndCarnivorous.type]
+  , [2, cardsData.CardMetamorphoseAndSpecA.type]
+  , [2, cardsData.CardViviparousAndSpecB.type]
+//3
+//живорождение / водоплавающее - 2,
+//трематода / сотрудничество - 4,
+//трематода / взаимодействие - 2,
+//трематода / жировой запас - 2.
+  , [2, cardsData.CardViviparousAndSwimming.type]
+  , [4, cardsData.CardTrematodeAndCommunication.type]
+  , [2, cardsData.CardTrematodeAndCooperation.type]
+  , [2, cardsData.CardTrematodeAndFatTissue.type]
 ];
