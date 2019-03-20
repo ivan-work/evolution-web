cec45a4ae4836ad81a4c1b78702b72100bc619e1
ivan <no>
2019-03-20 20:00:48 +0300

    server webpack update

--- webpack.server.babel.js
+++ webpack.server.babel.js
@@ -14,7 +14,8 @@ const nodeModules = fs.readdirSync('node_modules')
   .reduce((result, mod) => Object.assign(result, {[mod]: 'commonjs ' + mod}, {}));
 
 export default {
-  devtool: isDevelopment ? 'source-map' : 'source-map'
+  mode: 'none'
+  , devtool: isDevelopment ? 'source-map' : 'source-map'
   , entry: isDevelopment ? ['./server/index.js'] : ['./server/index.js']
   , output: {
     path: path.join(__dirname, 'dist/server/')
